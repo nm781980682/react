d261e548adb6bdbfb80cc9e5b4d92677d531fa76
nm781980682 <781980682@qq.com>
2018-08-27 18:46:50 +0800

    56478

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Button from './components/button'
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Button value="按钮" />
+        <Button value="按钮" />
+        <Button value="按钮" />
+      </div>
+    );
+  }
+}
+
+export default App;
